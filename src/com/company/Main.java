46ca814f088c8cec2ecbe59cc9935b16b9841113
alina <alinatitova@gmail.com>
2018-02-15 20:59:09 +0300

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a=3;
+	int b=11;
+	int c=(a+b)*2;
+	System.out.println(c);
+	int d=a/b;
+        System.out.println(d);
+        float f=20.f/3.f;
+        System.out.println(f);
+
+
+
+
+
+
+    }
+}
